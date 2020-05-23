8d59bbcb5a77c6bb61452d99544b6d0c74cdb8b3
fwenhua <langrenbule@gmail.com>
2020-05-23 18:29:08 +0800

    新增 swagger的使用示例

--- pandora-demo/src/main/java/com/hello/demo/controller/HelloController.java
+++ pandora-demo/src/main/java/com/hello/demo/controller/HelloController.java
@@ -1,15 +1,16 @@
 package com.hello.demo.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
+@Api(tags = "示例模块")
 public class HelloController {
 
-
     @RequestMapping("/")
-
+    @ApiOperation(value = "欢迎入口",notes = "示例swagger接口的使用")
     public String index(){
         return "hello world";
     }
